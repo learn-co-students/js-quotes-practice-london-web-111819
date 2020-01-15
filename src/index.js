3204cfa1f44ab71dccbcfc6c22c380579408d426
badogan <basri.dogan.71@gmail.com>
2020-01-15 09:52:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-//HELPER APIS
+
+    //HELPER APIS
 function get(URI) {
     return fetch(URI).then(response=>response.json())
 }
@@ -24,86 +25,96 @@ function post(URI,newObj){
     return fetch(URI, configObj).then(response=>response.json())
 }
 
-//CONSTANTS
-const QUOTES_BASE_URL="http://localhost:3000/quotes"
-const LIKES_BASE_URL="http://localhost:3000/likes"
-const QUOTE_LIST = document.getElementById("quote-list")
-const NEW_QUOTE_FORM = document.getElementById("new-quote-form")
-
-//FUNCTIONS
-function populatePageWithExistingQuotes(){
-    get(`${QUOTES_BASE_URL}?_embed=likes`).then(quotes=>quotes.forEach(renderQuoteToPage))
+function patch(URI,id,patchObj){
+    let patchData = {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+        body: JSON.stringify(patchObj)
+        };
+        return fetch(`${URI}${id}`,patchData).then(response=>response.json())
 }
+    // CONSTANTS
+    const QUOTE_LIST = document.getElementById("quote-list")
+    const QUOTES_BASE_URI = "http://localhost:3000/quotes"
+    const NEW_QUOTE_FORM = document.getElementById("new-quote-form")
+    const LIKES_BASE_URI = "http://localhost:3000/likes"
 
-function renderQuoteToPage(quote){
-    let newLi = document.createElement('li')
-    newLi.classList.add('quote-card')
-    let newBlockQuote = document.createElement('blockquote')
-    newBlockQuote.classList.add('blockquote')
-    let newP = document.createElement('p')
-    newP.innerText = quote.quote
-    let newFooter = document.createElement('footer')
-    newFooter.classList.add("blockquote-footer")
-    newFooter.innerText = quote.author
-    let newBreak = document.createElement("br")
-    let newLikesButton = document.createElement('button')
-    newLikesButton.innerText = `Likes: ${quote.likes.length}`
-    newLikesButton.classList.add("btn-success")
-    newLikesButton.addEventListener("click",function(event){
-        addLikesAndUpdatePage(quote.id,newLikesButton)
-    })
-    let newDeleteButton = document.createElement('button')
-    newDeleteButton.innerText = "Delete"
-    newDeleteButton.classList.add("btn-danger")
-    newDeleteButton.addEventListener("click",function(event){
-        deleteQuoteAndRemoveFromPage(quote.id,newLi)
-    })
-    newBlockQuote.append(newP,newFooter,newBreak,newLikesButton,newDeleteButton)
-    newLi.appendChild(newBlockQuote)
-    QUOTE_LIST.appendChild(newLi)
-}
-
-function deleteQuoteAndRemoveFromPage(id,elementToRemove){
-    destroy(QUOTES_BASE_URL,id).then(()=>elementToRemove.remove())
-}
-
-function addLikesAndUpdatePage(id,elementToWorkWith){
-    let newQuoteId = id
-    let newObj = {
-        quoteId: newQuoteId
+    // FUNCTIONS
+    function bringALLTheQuotesToPage(){
+        let uriToSend
+        uriToSend = `${QUOTES_BASE_URI}?_embed=likes`
+        get(uriToSend).then(processReceivedQuotes)
     }
-    post(LIKES_BASE_URL,newObj).then(newLike=>processNewLike(newLike,elementToWorkWith))
-}
 
-function processNewLike(newLike,elementToWorkWith){
-    currentCountOfLikes = parseInt(elementToWorkWith.innerText.slice(6))
-    elementToWorkWith.innerText = `Likes: ${currentCountOfLikes+1}`
-}
-
-// function provideNumberOfLikes(quoteId){
-//     // return get(LIKES_BASE_URL).then(likesArray=>countForThisId(likesArray,quoteId))
-//     return get(LIKES_BASE_URL).then(likesArray=>function(likesArray){
-//         console.log(likesArray)
-//         debugger
-//     })
-// }
-
-
-
-//LOADERS
-populatePageWithExistingQuotes()
-
-
-NEW_QUOTE_FORM.addEventListener("submit",function(event){
-    let newQuoteText = document.getElementById("new-quote").value
-    let newAuthor = document.getElementById("author").value
-    let newObj = {
-        quote: newQuoteText,
-        author: newAuthor 
+    function processReceivedQuotes(allQuotes){
+        allQuotes.forEach(putQuoteOnPage)
     }
-    post(QUOTES_BASE_URL,newObj).then(quote=>renderQuoteToPage(quote))
-})
 
+    function putQuoteOnPage(quote){
+        let newLi = document.createElement('li')
+        newLi.classList.add("quote-card")
+        let newBlockquote = document.createElement('newBlockquote')
+        let newP = document.createElement('p')
+        newP.classList.add("mb-0")
+        newP.innerText = quote.quote
+        let newFooter = document.createElement('footer')
+        newFooter.classList.add("blockquote-footer")
+        newFooter.innerText = quote.author
+        let newBreak = document.createElement('break')
+        let newLikesButton = document.createElement('button')
+        newLikesButton.classList.add("btn-success")
+        newLikesButton.innerText = "Likes: "
+        newLikesButton.addEventListener("click",()=>addLikeAndShowInTheButton(quote,newSpan))
+        let newSpan = document.createElement('span')
+        if (quote.likes != null){
+            newSpan.innerText = quote.likes.length
+        } else {
+            newSpan.innerText = 0
+        }
+        let newDeleteButton = document.createElement('button')
+        newDeleteButton.classList.add("btn-danger")
+        newDeleteButton.innerText = "Delete"
+        newDeleteButton.addEventListener("click",()=>deleteQuoteAndRemoveFromPage(quote,newLi))
+        //Appends right? yes
+        newLikesButton.appendChild(newSpan)
+        newBlockquote.append(newP,newFooter,newBreak,newLikesButton,newDeleteButton)
+        newLi.appendChild(newBlockquote)
+        QUOTE_LIST.appendChild(newLi)
+    }
 
+    function addLikeAndShowInTheButton(quote,spanElementToUpdate){
+        event.preventDefault()
+        let urlTOSend = LIKES_BASE_URI
+        let configObj = {
+            quoteId: quote.id
+        }
+        post(urlTOSend,configObj).then(()=>updateShowButton(spanElementToUpdate))
+    }
+
+    function updateShowButton(spanElementToUpdate){
+        spanElementToUpdate.innerText = parseInt(spanElementToUpdate.innerText) + 1 
+    }
+
+    function deleteQuoteAndRemoveFromPage(quote,elementToRemove){
+        destroy(QUOTES_BASE_URI,quote.id).then(elementToRemove.remove())
+    }
+
+    function addANewQuoteAndAddToTheQuotesOnPage(event){
+        event.preventDefault();
+        let newQuoteText = document.getElementById("new-quote").value
+        let newQuoteAuthor = document.getElementById("author").value
+        let newObjectToSend = {
+            quote: newQuoteText,
+            author: newQuoteAuthor
+        }
+        post(QUOTES_BASE_URI,newObjectToSend).then(newQuote=>putQuoteOnPage(newQuote))
+    }
+
+    //EVENTLISTENERS, INITIAL LOADERS
+    bringALLTheQuotesToPage()
+    NEW_QUOTE_FORM.addEventListener("submit",addANewQuoteAndAddToTheQuotesOnPage)
 
 })
